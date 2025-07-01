5de687ca7785df6dc71377632792583bf656464f
eraykkoc <123729330+eraykkoc@users.noreply.github.com>
2025-07-01 21:55:42 +0300

    Değişiklik mesajı

--- uni_yillik/settings.py
+++ uni_yillik/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import resend
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / '.env'
